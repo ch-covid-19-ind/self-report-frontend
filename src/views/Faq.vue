075094941c72628a51bee5b1a7edaaf15dfcbdb0
Prabhakaran Kasinathan <prabhakaran1989@gmail.com>
2020-03-29 19:51:07 +0200

    improved faq and about us

--- src/views/Faq.vue
+++ src/views/Faq.vue
@@ -28,6 +28,9 @@
             <h3 class="text-white">{{ $t(`faq.othersQuestion4`, {disease: 'Covid-19'}) }}</h3>
             <p>{{ $t(`faq.othersResponse4`, {disease: 'Covid-19'}) }}</p>
 
+            <h3 class="text-white">{{ $t(`faq.othersQuestion5`, {disease: 'Covid-19'}) }}</h3>
+            <p>{{ $t(`faq.othersResponse5`, {disease: 'Covid-19'}) }}</p>
+
           </div>
         </div>
       </section>
